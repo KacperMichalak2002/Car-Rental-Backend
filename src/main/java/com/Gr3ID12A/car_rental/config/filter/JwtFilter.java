f9e89b8fdd2881ad550b42b238a23a4b3fed4963
Kacper Michalak <michalakkacper2002@gmail.com>
2025-06-15 19:17:52 +0200

    Added token exceptions handling server returns 401

--- src/main/java/com/Gr3ID12A/car_rental/config/filter/JwtFilter.java
+++ src/main/java/com/Gr3ID12A/car_rental/config/filter/JwtFilter.java
@@ -38,27 +38,29 @@ public class JwtFilter extends OncePerRequestFilter {
             token = authHeader.substring(7);
             try{
                 username = jwtService.extractUsername(token);
+
+                if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                    boolean isTokenValid = tokenRepository.findByToken(token)
+                            .map(t -> !t.isExpired() && !t.isRevoked())
+                            .orElse(false);
+
+                    if(jwtService.validateToken(token, userDetails) && isTokenValid){
+                        UsernamePasswordAuthenticationToken authToken =
+                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
+                }
             }catch (Exception e){
                 log.error("Error extracting username {}",e.getMessage());
             }
 
         }
 
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
 
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-            boolean isTokenValid = tokenRepository.findByToken(token)
-                    .map(t -> !t.isExpired() && !t.isRevoked())
-                    .orElse(false);
-
-            if(jwtService.validateToken(token, userDetails) && isTokenValid){
-                UsernamePasswordAuthenticationToken authToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-        }
 
         filterChain.doFilter(request,response);
 
